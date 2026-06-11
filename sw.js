52787f355356a4dccb655867569f658851b7040e
shehabziada130 <84864669+shehabziada130@users.noreply.github.com>
2026-06-11 06:26:42 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'axis-v4';
+const CACHE = 'axis-v1.5';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
